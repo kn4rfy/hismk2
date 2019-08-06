6e298a13d837c61375fb75fa34d2738f7687eb4c
Francis Knarfy Elopre <kn4rfy@gmail.com>
2019-08-06 10:56:31 +0800

    list

---
+++ src/main/resources/config/liquibase/sample_data/sample_employees.sql
@@ -0,0 +1,156 @@
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('47742949-d663-4ce8-b5df-039a56bd83b4', '1096', 'Lacy', 'Fradson', 'Gallehawk', 'III', '703 Starling Court',
+        'United States', 'Pennsylvania', 'Philadelphia', 'Phoenix', 'Female', '1926-05-23', 'Lacy Gallehawk',
+        '36621 Charing Cross Circle', 'Grandfather', '215-481-8734', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('5d7fe492-10b1-4ccb-b725-542347412abb', '1460', 'Jim', 'Curl', 'Beller', 'IV', '644 Arrowood Avenue',
+        'United States', 'Delaware', 'Newark', 'Albany', 'Male', '1952-03-28', 'Jim Beller', '516 Fairview Circle',
+        'Sister', '302-158-6413', 2, null, '2019-08-06 10:53:23.576659', null, '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('b3e2baf0-94b1-4d5c-a44c-1182c97b4b0b', '1494', 'Ryley', 'Kuna', 'Curlis', 'IV', '7 Declaration Drive',
+        'United States', 'North Carolina', 'Durham', 'Las Vegas', 'Male', '1982-07-16', 'Ryley Curlis',
+        '19460 Portage Hill', 'Son', '919-574-7126', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('ad06b1de-8dec-401b-bebb-78e5b9ca3c29', '1415', 'Justine', 'O''Beirne', 'Abbotson', null, '3 Jenna Way',
+        'United States', 'Massachusetts', 'Boston', 'Beaufort', 'Female', '1995-01-06', 'Justine Abbotson',
+        '841 Carpenter Street', 'Aunt', '617-660-2892', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('36bf81c6-1a28-4d38-93a7-500fdc9df5f6', '1056', 'Pinchas', 'Jeggo', 'Wybern', 'III', '60 Bartillon Pass',
+        'United States', 'Indiana', 'Fort Wayne', 'Cleveland', 'Male', '1947-04-22', 'Pinchas Wybern',
+        '19671 Trailsway Crossing', 'Friend', '260-709-8520', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('8ada564e-f254-470f-a95b-f084b645420a', '1040', 'Jarvis', 'Blazhevich', 'Rose', null, '9 Lyons Crossing',
+        'United States', 'Indiana', 'Fort Wayne', 'Muncie', 'Male', '1980-07-22', 'Jarvis Rose', '54341 Grover Alley',
+        'Grandmother', '260-335-2223', 2, null, '2019-08-06 10:53:23.576659', null, '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('2eebb8cf-bebe-497f-adad-a2b375e5c209', '1910', 'Fredek', 'Clayton', 'Balkwill', 'II', '96231 Magdeline Trail',
+        'United States', 'New York', 'Syracuse', 'Spring Hill', 'Male', '1927-04-13', 'Fredek Balkwill',
+        '627 Rusk Center', 'Mother', '315-198-7665', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('27bd5154-563b-4741-82c9-fb48941462f1', '1614', 'Say', 'Blannin', 'Karpets', 'III', '58817 Debs Lane',
+        'United States', 'Florida', 'Orlando', 'Washington', 'Male', '1929-04-29', 'Say Karpets', '175 Thierer Court',
+        'Daughter', '407-641-2365', 2, null, '2019-08-06 10:53:23.576659', null, '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('09cce322-3aa8-41da-8571-fd850ba79072', '1114', 'Rolfe', 'Iredale', 'Etherton', 'Jr',
+        '72179 Burning Wood Drive', 'United States', 'Idaho', 'Boise', 'Huntsville', 'Male', '1945-02-23',
+        'Rolfe Etherton', '06 Crownhardt Lane', 'Uncle', '208-868-6110', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('18ac5a27-371a-4ed9-8d15-32f371d9bc1a', '1599', 'Marline', 'Snead', 'Donaghie', null, '985 Superior Parkway',
+        'United States', 'New York', 'White Plains', 'Sioux City', 'Female', '1997-06-04', 'Marline Donaghie',
+        '043 Center Alley', 'Mother', '914-853-9153', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('733b11ed-265b-4b98-bc41-dc00ef06279b', '1818', 'Aharon', 'Ovanesian', 'Bravery', null,
+        '17650 Northwestern Terrace', 'United States', 'Virginia', 'Roanoke', 'Lexington', 'Male', '1961-08-26',
+        'Aharon Bravery', '0960 Linden Avenue', 'Uncle', '540-665-2519', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('f1087d09-79c2-4ac6-931b-15811208c702', '1830', 'Lynna', 'Sail', 'Dumphy', null, '3 Sloan Park',
+        'United States', 'Michigan', 'Detroit', 'Oklahoma City', 'Female', '1984-07-30', 'Lynna Dumphy',
+        '675 Buena Vista Park', 'Mother', '313-851-7262', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('df982c8a-0082-4498-af32-23d91dfc9a6c', '1338', 'Sapphira', 'Papis', 'Weatherby', 'IV', '836 Myrtle Terrace',
+        'United States', 'Texas', 'San Antonio', 'Topeka', 'Female', '1961-07-17', 'Sapphira Weatherby',
+        '40782 Messerschmidt Circle', 'Mother', '210-626-1306', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('60230329-33c1-4d53-89be-c92f6f2d202b', '1248', 'Rhett', 'Wines', 'Adamo', null, '12 Brentwood Road',
+        'United States', 'Illinois', 'Chicago', 'Burbank', 'Male', '1946-11-02', 'Rhett Adamo', '978 Toban Lane',
+        'Daughter', '312-541-6640', 2, null, '2019-08-06 10:53:23.576659', null, '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('3b2e0f5c-e6cf-4c09-8c47-53f783912a4c', '1844', 'Cris', 'Shackle', 'Simper', 'III', '7 1st Crossing',
+        'United States', 'Indiana', 'Indianapolis', 'Young America', 'Male', '1968-08-16', 'Cris Simper',
+        '04 Towne Junction', 'Uncle', '317-877-2032', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('b2a3df70-16fc-4674-bd43-e0ecc58f3d8f', '1727', 'Caryl', 'Buxsey', 'Vannah', null, '018 Pierstorff Place',
+        'United States', 'Arkansas', 'Little Rock', 'Sacramento', 'Male', '2002-02-25', 'Caryl Vannah',
+        '325 Sycamore Alley', 'Uncle', '501-944-8026', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('1c8f5672-3388-4db3-b086-d18e01ce2182', '1441', 'Pedro', 'Robertacci', 'Boxill', 'II', '61 Artisan Court',
+        'United States', 'Texas', 'Fort Worth', 'High Point', 'Male', '1928-11-11', 'Pedro Boxill',
+        '60310 Mosinee Point', 'Brother', '817-420-3013', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('0032acf8-71c5-4f9b-add4-530ad4ce04f5', '1553', 'Elbertina', 'Antosch', 'Cowterd', null, '2 Talisman Street',
+        'United States', 'New York', 'Schenectady', 'Allentown', 'Female', '1985-03-12', 'Elbertina Cowterd',
+        '118 Maywood Center', 'Friend', '518-216-0366', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('8f614e99-2c7f-4d9a-9747-0104d8199e45', '1660', 'Dody', 'Haddy', 'Grishakov', null, '1 Vidon Park',
+        'United States', 'Kentucky', 'Louisville', 'Colorado Springs', 'Female', '1974-11-24', 'Dody Grishakov',
+        '57842 Warrior Hill', 'Father', '502-354-6640', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
+INSERT INTO hrm.employees (id, employee_no, first_name, last_name, middle_name, name_suffix, address, country,
+                           state_province, city_municipality, barangay, gender, dob, emergency_contact_name,
+                           emergency_contact_address, emergency_contact_relationship, emergency_contact_no, "user",
+                           created_by, created_date, last_modified_by, last_modified_date)
+VALUES ('e74f72b2-198b-45bf-827e-91ce4ac141be', '1373', 'Marshall', 'Denizet', 'Abrami', 'II',
+        '21463 Coolidge Junction', 'United States', 'Virginia', 'Norfolk', 'Kent', 'Male', '1908-02-20',
+        'Marshall Abrami', '47 Toban Trail', 'Uncle', '757-149-6975', 2, null, '2019-08-06 10:53:23.576659', null,
+        '2019-08-06 10:53:23.576659');
