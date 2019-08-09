d6e2fdad5768d294a3afa30ff721698cd0b41957
crisnil <cacuyado@gmail.com>
2019-08-09 10:41:42 +0800

    ancillary schema

--- src/main/groovy/com/hisd3/hismk2/repository/Department.groovy
+++ src/main/groovy/com/hisd3/hismk2/repository/Department.groovy
@@ -1,6 +1,6 @@
 package com.hisd3.hismk2.repository
 
-import com.hisd3.hismk2.domain.ancillary.RevenueCenter
+
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
