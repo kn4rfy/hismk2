c1375ad465e5df020d268328aba7f4cf8adc4c93
ayingjohnpaul <ayingjohnpaul@gmail.com>
2019-08-15 17:08:25 +0800

    receiving report.

--- src/main/groovy/com/hisd3/hismk2/graphqlservices/inventory/ReceivingReportService.groovy
+++ src/main/groovy/com/hisd3/hismk2/graphqlservices/inventory/ReceivingReportService.groovy
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.hisd3.hismk2.dao.inventory.ReceivingDao
 import com.hisd3.hismk2.domain.inventory.ReceivingReport
 import com.hisd3.hismk2.domain.inventory.ReceivingReportItem
+import com.hisd3.hismk2.domain.pms.Patient
+import com.hisd3.hismk2.repository.inventory.ReceivingReportItemRepository
 import com.hisd3.hismk2.repository.inventory.ReceivingReportRepository
 import io.leangen.graphql.annotations.GraphQLArgument
 import io.leangen.graphql.annotations.GraphQLContext
@@ -22,7 +24,7 @@ class ReceivingReportService {
 	
 	@Autowired
 	ReceivingDao receivingDao
-	
+
 	@Autowired
 	ObjectMapper objectMapper
 	
@@ -40,13 +42,32 @@ class ReceivingReportService {
 	ReceivingReport getReceivingById(@GraphQLArgument(name = "id") UUID id) {
 		return receivingDao.getReceivingReport(id)
 	}
-	
+
 	@GraphQLMutation
 	ReceivingReport addReceivingReport(
 			@GraphQLArgument(name = "fields") Map<String, Object> fields
-	) {
+	){
 		def receiving = objectMapper.convertValue(fields, ReceivingReport)
 		return receivingDao.save(receiving)
+	}
+
+
+    @GraphQLMutation
+    ReceivingReport deleteReceivingReportItems(
+            @GraphQLArgument(name="id") UUID id,
+            @GraphQLArgument(name = "items") List<Map<String, Object>> items
+    ){
+
+        return receivingDao.deleteItems(id, items)
+    }
+
+	@GraphQLMutation
+	ReceivingReport addReceivingReportItems(
+			@GraphQLArgument(name="id") UUID id,
+			@GraphQLArgument(name = "item") Map<String, Object> item
+	){
+		def receivingItem = objectMapper.convertValue(item, ReceivingReportItem)
+		return receivingDao.addItems(id, receivingItem)
 	}
 	
 }
