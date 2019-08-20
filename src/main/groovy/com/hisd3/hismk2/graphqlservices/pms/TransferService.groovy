188325db268d84be76debdbc2450be72e5d6c783
acejordan <acelumaad@live.com>
2019-08-20 10:01:37 +0800

    updates dao

--- src/main/groovy/com/hisd3/hismk2/graphqlservices/pms/TransferService.groovy
+++ src/main/groovy/com/hisd3/hismk2/graphqlservices/pms/TransferService.groovy
@@ -3,9 +3,11 @@ package com.hisd3.hismk2.graphqlservices.pms
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.hisd3.hismk2.dao.DepartmentDao
 import com.hisd3.hismk2.dao.bms.RoomDao
+import com.hisd3.hismk2.dao.pms.CaseDao
 import com.hisd3.hismk2.dao.pms.TransferDao
 import com.hisd3.hismk2.domain.Department
 import com.hisd3.hismk2.domain.bms.Room
+import com.hisd3.hismk2.domain.pms.Case
 import com.hisd3.hismk2.domain.pms.Transfer
 import com.hisd3.hismk2.services.GeneratorService
 import io.leangen.graphql.annotations.GraphQLArgument
@@ -18,79 +20,88 @@ import org.springframework.stereotype.Component
 @Component
 @GraphQLApi
 class TransferService {
-	
-	@Autowired
-	TransferDao transferDao
-	
-	@Autowired
-	DepartmentDao departmentDao
-	
-	@Autowired
-	RoomDao roomDao
-	
-	@Autowired
-	GeneratorService generatorService
-	
-	@Autowired
-	ObjectMapper objectMapper
-	
-	//============== All Queries ====================
-	
-	@GraphQLQuery(name = "transfers", description = "Get All Transfers")
-	Set<Transfer> findAll() {
-		transferDao.findAll()
-	}
-	
-	@GraphQLQuery(name = "searchTransfers", description = "Search transfers")
-	List<Transfer> searchTransfers(@GraphQLArgument(name = "filter") String filter) {
-		transferDao.searchTransfers(filter)
-	}
-	
-	@GraphQLQuery(name = "getTransfersByCase", description = "Transfers by case")
-	List<Transfer> getTransfersByCase(@GraphQLArgument(name = "caseNo") String caseNo) {
-		transferDao.getTransfersByCase(caseNo)
-	}
-	
-	@GraphQLQuery(name = "transfer", description = "Get Transfer By Id")
-	Transfer findById(@GraphQLArgument(name = "id") String id) {
-		return transferDao.findById(id)
-	}
-	
-	//============== All Mutations ====================
-	
-	@GraphQLMutation
-	Transfer upsertTransfer(
-			@GraphQLArgument(name = "id") String id,
-			@GraphQLArgument(name = "fields") Map<String, Object> fields
-	) {
-		
-		if (id) {
-			def transfer = transferDao.findById(id)
-			objectMapper.updateValue(transfer, fields)
-			
-			def departmentId = fields["departmentId"]
-			Department department = departmentDao.findById(departmentId as String)
-			transfer.department = department
-			
-			def roomId = fields["roomId"]
-			Room room = roomDao.findById(roomId as String)
-			transfer.room = room
-			
-			return transferDao.save(transfer)
-		} else {
-			def transfer = objectMapper.convertValue(fields, Transfer)
-			
-			def departmentId = fields["departmentId"]
-			Department department = departmentDao.findById(departmentId as String)
-			transfer.department = department
-			
-			if (fields["roomId"] != null) {
-				def roomId = fields["roomId"]
-				Room room = roomDao.findById(roomId as String)
-				transfer.room = room
-			}
-			
-			return transferDao.save(transfer)
-		}
-	}
+
+    @Autowired
+    TransferDao transferDao
+
+    @Autowired
+    CaseDao caseDao
+
+    @Autowired
+    DepartmentDao departmentDao
+
+    @Autowired
+    RoomDao roomDao
+
+    @Autowired
+    GeneratorService generatorService
+
+    @Autowired
+    ObjectMapper objectMapper
+
+    //============== All Queries ====================
+
+    @GraphQLQuery(name = "transfers", description = "Get All Transfers")
+    Set<Transfer> findAll() {
+        transferDao.findAll()
+    }
+
+    @GraphQLQuery(name = "searchTransfers", description = "Search transfers")
+    List<Transfer> searchTransfers(@GraphQLArgument(name = "filter") String filter) {
+        transferDao.searchTransfers(filter)
+    }
+
+    @GraphQLQuery(name = "getTransfersByCase", description = "Transfers by case")
+    List<Transfer> getTransfersByCase(@GraphQLArgument(name = "caseNo") String caseNo) {
+        transferDao.getTransfersByCase(caseNo)
+    }
+
+    @GraphQLQuery(name = "transfer", description = "Get Transfer By Id")
+    Transfer findById(@GraphQLArgument(name = "id") String id) {
+        return transferDao.findById(id)
+    }
+
+    //============== All Mutations ====================
+
+    @GraphQLMutation
+    Transfer upsertTransfer(
+            @GraphQLArgument(name = "id") String id,
+            @GraphQLArgument(name = "fields") Map<String, Object> fields
+    ) {
+
+        if (id) {
+            def transfer = transferDao.findById(id)
+            objectMapper.updateValue(transfer, fields)
+
+            def departmentId = fields["departmentId"]
+            Department department = departmentDao.findById(departmentId as String)
+            transfer.department = department
+
+            def roomId = fields["roomId"]
+            Room room = roomDao.findById(roomId as String)
+            transfer.room = room
+
+            return transferDao.save(transfer)
+        } else {
+            def transfer = objectMapper.convertValue(fields, Transfer)
+
+            def departmentId = fields["departmentId"]
+            Department department = departmentDao.findById(departmentId as String)
+            transfer.department = department
+            if(fields["roomId"] != null) {
+                def roomId = fields["roomId"]
+                Room room = roomDao.findById(roomId as String)
+                transfer.room = room
+            }
+
+            def returnVal = transferDao.save(transfer)
+
+            Case pCase = caseDao.findByCaseNo(fields["caseNo"] as String)
+            pCase.registryType = fields["registryType"] as String
+            caseDao.save(pCase)
+
+            return returnVal
+
+        }
+    }
 }
