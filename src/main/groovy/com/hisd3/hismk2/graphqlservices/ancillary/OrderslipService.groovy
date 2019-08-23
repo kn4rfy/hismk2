8859bd2e5530870f48f5df7a48b21b0b0d58f3a5
crisnil <cacuyado@gmail.com>
2019-08-23 09:17:17 +0800

    aorderslip gql service

--- src/main/groovy/com/hisd3/hismk2/graphqlservices/ancillary/OrderslipService.groovy
+++ src/main/groovy/com/hisd3/hismk2/graphqlservices/ancillary/OrderslipService.groovy
@@ -6,16 +6,12 @@ import com.hisd3.hismk2.dao.ancillary.dto.DiagnosticsResults
 import com.hisd3.hismk2.domain.Department
 import com.hisd3.hismk2.domain.ancillary.Orderslip
 import com.hisd3.hismk2.services.GeneratorService
-import com.hisd3.hismk2.services.GeneratorType
 import io.leangen.graphql.annotations.GraphQLArgument
 import io.leangen.graphql.annotations.GraphQLMutation
 import io.leangen.graphql.annotations.GraphQLQuery
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi
-import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-
-import java.time.LocalDateTime
 
 @Component
 @GraphQLApi
@@ -36,9 +32,8 @@ class OrderslipService {
 	List<Orderslip> findAll() {
 		orderslipDao.findAll()
 	}
-
+	
 	@GraphQLQuery(name = "orderslipsByCase", description = "Get All Orderslips by case")
-
     List<DiagnosticsResults> findByCase(
 			@GraphQLArgument(name = "id") String id
 	) {
@@ -52,17 +47,17 @@ class OrderslipService {
 			@GraphQLArgument(name = "departmentId") String departmentId
 	) {
 			return orderslipDao.findByCaseAndDepartment(id,departmentId)
+
 	}
-
-
+	
 	//============== All Mutations ====================
-
+	
 	@GraphQLMutation
 	Orderslip addOrderslip(
 			@GraphQLArgument(name = "fields") Map<String, Object> fields
-	){
+	) {
 		println(fields)
 		return orderslipDao.addOrderslip(fields)
 	}
-
+	
 }
