a3c6ab84a91e819b6995a7d95527251fbb90c8ec
ayingjohnpaul <ayingjohnpaul@gmail.com>
2019-08-28 10:57:15 +0800

    purchase request [partial]

---
+++ src/main/groovy/com/hisd3/hismk2/repository/inventory/PurchaseRequestRepository.groovy
@@ -0,0 +1,9 @@
+package com.hisd3.hismk2.repository.inventory
+
+import com.hisd3.hismk2.domain.inventory.PurchaseRequest
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest, UUID> {
+
+
+}
