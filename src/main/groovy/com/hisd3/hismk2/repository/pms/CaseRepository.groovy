25f32d0ad1ef8297c2f9ab9804086cc6f713d447
acejordan <acelumaad@live.com>
2019-08-29 11:02:45 +0800

    case sorting

--- src/main/groovy/com/hisd3/hismk2/repository/pms/CaseRepository.groovy
+++ src/main/groovy/com/hisd3/hismk2/repository/pms/CaseRepository.groovy
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param
 interface CaseRepository extends JpaRepository<Case, UUID> {
 	
 	@Query(
-			value = "Select c from Case c where c.patient.id = :patientId and c.status = 'ACTIVE'"
+			value = "Select c from Case c where c.patient.id = :patientId and c.status = 'ACTIVE' order by c.entryDatetime desc"
 	)
 	Case getPatientActiveCase(@Param("patientId") UUID patientId)
 	
@@ -19,6 +19,6 @@ interface CaseRepository extends JpaRepository<Case, UUID> {
 	)
 	Case findByCaseNo(@Param("caseNo") String caseNo)
 	
-	@Query(value = '''Select c from Case c where c.patient.id = :patientId order by c.entryDatetime''')
+	@Query(value = '''Select c from Case c where c.patient.id = :patientId order by c.entryDatetime desc''')
 	List<Case> getPatientCases(@Param("patientId") UUID patientId)
 }
