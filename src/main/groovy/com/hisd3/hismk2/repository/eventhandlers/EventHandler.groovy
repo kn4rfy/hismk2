d583feac38718889d07fea609757f7d7b3c9ae35
greggianbayocboc <greggianbayocboc@gmail.com>
2019-09-03 15:00:57 +0800

    Knarfy pahak auditing

--- src/main/groovy/com/hisd3/hismk2/repository/eventhandlers/EventHandler.groovy
+++ src/main/groovy/com/hisd3/hismk2/repository/eventhandlers/EventHandler.groovy
@@ -1,5 +1,7 @@
 package com.hisd3.hismk2.repository.eventhandlers
 
+import com.hisd3.hismk2.domain.inventory.StockRequest
+import com.hisd3.hismk2.domain.inventory.StockRequestItem
 import com.hisd3.hismk2.domain.pms.Case
 import com.hisd3.hismk2.domain.pms.Patient
 import com.hisd3.hismk2.services.GeneratorService
@@ -8,6 +10,7 @@ import groovy.transform.TypeChecked
 import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.rest.core.annotation.HandleAfterCreate
+import org.springframework.data.rest.core.annotation.HandleAfterSave
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler
 
@@ -58,5 +61,28 @@ class EventHandler {
 			
 		}
 	}
-	
+
+	@HandleAfterSave
+	handleMedicationAfterSave(StockRequest stockRequest)
+	{
+		if(stockRequest.status=="CLAIMABLE")
+		{
+			println("MUST NOTIFY THAT REQUEST IS CLAIMABLE")
+			for(StockRequestItem stockRequestItem in stockRequest.stockRequestItems)
+			{
+				if(stockRequestItem.itemReferenceId!=null)
+				{
+					//deduct on inventory
+				}
+				else
+				{
+					println("Item has no inventory reference id and cannot charge or deduct on inventory : " + stockRequestItem.itemDescription)
+				}
+			}
+		}
+		if(stockRequest.status=="CLAIMED")
+		{
+			println("MUST CHARGE MEDCS")
+		}
+	}
 }
