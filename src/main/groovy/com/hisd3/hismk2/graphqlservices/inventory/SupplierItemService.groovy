a7d91ad125e83b280a5221f1f1c904851b191cb2
Francis Knarfy Elopre <kn4rfy@gmail.com>
2019-10-02 11:42:26 +0800

    chore: code review

--- src/main/groovy/com/hisd3/hismk2/graphqlservices/inventory/SupplierItemService.groovy
+++ src/main/groovy/com/hisd3/hismk2/graphqlservices/inventory/SupplierItemService.groovy
@@ -9,23 +9,23 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
-
 @Component
 @GraphQLApi
 @TypeChecked
 class SupplierItemService {
-
-    @Autowired SupplierItemRepository supplierItemRepository
-
-    @GraphQLQuery(name = "supplier_item_list", description = "List of Supplies")
-    List<SupplierItem> allSupplyItems() {
-        return supplierItemRepository.findAll()
-    }
-
-    @GraphQLQuery(name = "supply_item", description = "Supply Item")
-    SupplierItem getSupplyItem(
-            @GraphQLArgument(name = "id") String id
-    ) {
-        return supplierItemRepository.findById(UUID.fromString(id)).get()
-    }
+	
+	@Autowired
+	SupplierItemRepository supplierItemRepository
+	
+	@GraphQLQuery(name = "supplier_item_list", description = "List of Supplies")
+	List<SupplierItem> allSupplyItems() {
+		return supplierItemRepository.findAll()
+	}
+	
+	@GraphQLQuery(name = "supply_item", description = "Supply Item")
+	SupplierItem getSupplyItem(
+			@GraphQLArgument(name = "id") String id
+	) {
+		return supplierItemRepository.findById(UUID.fromString(id)).get()
+	}
 }
