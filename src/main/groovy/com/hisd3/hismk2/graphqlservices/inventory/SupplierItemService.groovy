63060610041f46fb322dcf43adc9f73e47d73449
cacuyado@gmail.com <B@g^Quz%&1fYbu>
2019-10-02 11:21:46 +0800

    supplierItem

---
+++ src/main/groovy/com/hisd3/hismk2/graphqlservices/inventory/SupplierItemService.groovy
@@ -0,0 +1,31 @@
+package com.hisd3.hismk2.graphqlservices.inventory
+
+import com.hisd3.hismk2.domain.inventory.SupplierItem
+import com.hisd3.hismk2.repository.inventory.SupplierItemRepository
+import groovy.transform.TypeChecked
+import io.leangen.graphql.annotations.GraphQLArgument
+import io.leangen.graphql.annotations.GraphQLQuery
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+
+@Component
+@GraphQLApi
+@TypeChecked
+class SupplierItemService {
+
+    @Autowired SupplierItemRepository supplierItemRepository
+
+    @GraphQLQuery(name = "supplier_item_list", description = "List of Supplies")
+    List<SupplierItem> allSupplyItems() {
+        return supplierItemRepository.findAll()
+    }
+
+    @GraphQLQuery(name = "supply_item", description = "Supply Item")
+    SupplierItem getSupplyItem(
+            @GraphQLArgument(name = "id") String id
+    ) {
+        return supplierItemRepository.findById(UUID.fromString(id)).get()
+    }
+}
